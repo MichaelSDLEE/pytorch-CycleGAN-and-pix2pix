315e3d63e23e65bfd9440e2848bbfbcc901f0b37
xyang35 <yangxitongbob@gmail.com>
2017-04-30 13:21:50 -0400

    fix sorted bug

--- data/image_folder.py
+++ data/image_folder.py
@@ -25,8 +25,8 @@ def make_dataset(dir):
     images = []
     assert os.path.isdir(dir), '%s is not a valid directory' % dir
 
-    for root, _, fnames in sorted(os.walk(dir)):
-        for fname in fnames:
+    for root, _, fnames in os.walk(dir):
+        for fname in sorted(fnames):
             if is_image_file(fname):
                 path = os.path.join(root, fname)
                 images.append(path)
